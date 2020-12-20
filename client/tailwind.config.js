4aeb2af19110e8a88f7b4c6521447994e3d54c33
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-19 20:47:07 -0600

    feature(client): adding custom font

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -4,12 +4,9 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-      colors: {
-        primary: '#61DAFB',
-        secondary: '#282C34'
-      },
-      animation: {
-        'spin-slow': 'spin 20s linear infinite'
+      fontFamily: {
+        display: ['Raleway', 'serif'],
+        body: ['Roboto', 'sans-serif']
       }
     }
   },
