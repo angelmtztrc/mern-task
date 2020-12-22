31bc67284f0b5ace91d783213e3a2e511cd88fd6
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-22 14:26:33 -0600

    feature(server): hash the user password

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -1,9 +1,13 @@
+import argon2 from 'argon2';
+
+// models
 import User from '../models/user.model';
 
 export const create = async (req, res, next) => {
   const { name, email, password } = req.body;
 
   try {
+    // check if an user is already registered
     const userExists = await User.findOne({ email });
     if (userExists) {
       const error = new Error('User already registered');
@@ -11,10 +15,18 @@ export const create = async (req, res, next) => {
       next(error);
     }
 
-    const user = await new User({ name, email, password });
+    // hash the password
+    const passwordHashed = await argon2.hash(password);
+
+    // build a new user object
+    const user = await new User({ name, email, password: passwordHashed });
+
+    // save in database
     await user.save();
-    res.status(200).json({ type: 'success', data: user });
+
+    // return the response
+    res.status(200).json({ type: 'success', data: 'You registered successfully' });
   } catch (error) {
-    console.log(error);
+    next(error);
   }
 };
