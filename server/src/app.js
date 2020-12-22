ebdd8aa6f56b48380512e4bbe78210aaed89272e
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-21 23:30:06 -0600

    chore(server): morgan added

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 // routes
 import usersRoutes from './routes/users';
@@ -9,6 +10,9 @@ dotenv.config();
 
 // create an express application
 const app = express();
+
+// middlewares
+app.use(morgan('dev'));
 
 // routes
 app.use('/api/users', usersRoutes());
