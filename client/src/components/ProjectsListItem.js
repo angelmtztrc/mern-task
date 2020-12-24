4a3c2e26a6c881a95b354d0738e26812c43db1cb
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-24 16:52:37 -0600

    fix(client): change id for _id

--- client/src/components/ProjectsListItem.js
+++ client/src/components/ProjectsListItem.js
@@ -21,7 +21,7 @@ const ProjectsListItem = ({ project }) => {
     <li
       onClick={handleClick}
       className={`${
-        active && active.id === _id ? 'border-gray-800' : ''
+        active && active._id === _id ? 'border-gray-800' : ''
       } inline-block pl-2 text-gray-600 hover:text-gray-900 font-bold border-l-2 hover:border-gray-800 cursor-pointer transition-colors duration-300 ease-in`}
     >
       {title}
