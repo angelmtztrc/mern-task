b215b36547ef073a81260981154021c8f8ecae45
Angel Martinez <angel.martineztrc@gmail.com>
2020-12-24 16:24:53 -0600

    feature(client): show an screen if not project is selected

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,15 +1,18 @@
+import { useSelector } from 'react-redux';
+
 // components
 import Project from '../components/Project';
 import NotProjectSelected from '../components/NotProjectSelected';
 import Sidebar from '../components/Sidebar';
 
 const Dashboard = () => {
+  const { active } = useSelector(state => state.projects);
+
   return (
     <div className="grid grid-cols-12 min-h-screen">
       <Sidebar />
 
-      <Project />
-      {/* <NotProjectSelected /> */}
+      {active ? <Project /> : <NotProjectSelected />}
     </div>
   );
 };
